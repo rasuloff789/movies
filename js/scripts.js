902f9f206604dc95cc347da8a6925daf97448c67
rasuloff789 <rasuloff789@gmail.com>
2020-11-19 17:49:42 +0500

    project end

--- js/scripts.js
+++ js/scripts.js
@@ -11,17 +11,15 @@ var createMovieElement = function(movie){
   $_(".movie-imdb-id" , elNewMovie).textContent = movie.imdbId;
   $_(".movie-imdb-rating" , elNewMovie).textContent = movie.imdbRating;
   $_(".movie-description" , elNewMovie).textContent = `Brief description : ${movie.summary}`;
-  $_(".movie-runtime" , elNewMovie).textContent = movie.runtime + " minut";
+  $_(".movie-runtime" , elNewMovie).textContent = movie.runtime + " min";
   $_(".movie-language" , elNewMovie).textContent = movie.language;
   $_(".movie-genre" , elNewMovie).textContent = movie.categories.join(" , ");
   $_(".movie-genre" , elNewMovie).title = movie.categories.join(" , ");
-  
   return elNewMovie;
 };
 
 var normalizeMovies = movies.map(function(movie , i){
   return {
-    
     id: i + 1,
     title: movie.Title,
     year: movie.movie_year,
@@ -32,11 +30,10 @@ var normalizeMovies = movies.map(function(movie , i){
     runtime: movie.runtime,
     language: movie.language,
     youtubeId: `https://youtube.com/watch?v=${movie.ytid}`
-    
   };
 });
 
-var twoHundredVideos = normalizeMovies.slice(0 , 200);
+var twoHundredMovies = normalizeMovies.slice(0 , 200);
 
 var renderMovies = function (movies) {
   
@@ -50,42 +47,64 @@ var renderMovies = function (movies) {
   
   moviesList.appendChild(elMovieWrapperFragment);
 };
-renderMovies(twoHundredVideos);
+renderMovies(twoHundredMovies);
 
-var btnAddMovies  = $_(".js-btn-add-movies");
-
-btnAddMovies.addEventListener("click" , function(){
+var btnShowMovies  = $_(".js-btn-show-movies");
+btnShowMovies.addEventListener("click" , function(){
   renderMovies(normalizeMovies);
-  searchInput.value = "";
-  btnAddMovies.classList.add("d-none");
-  btnAddMovies.classList.remove("d-block");
+  btnShowMovies.classList.add("d-none");
+  btnShowMovies.classList.remove("d-block");
 });
 
 var btnReloadMovies = $_(".js-btn-reload-movies");
-
 btnReloadMovies.addEventListener("click" , function(){
-  renderMovies(twoHundredVideos);
-  searchInput.value = "";
+  renderMovies(twoHundredMovies);
   btnReloadMovies.classList.add("d-none");
   btnReloadMovies.classList.remove("d-block");
-  btnAddMovies.classList.add("d-block");
-  btnAddMovies.classList.remove("d-none");
+  btnShowMovies.classList.add("d-block");
+  btnShowMovies.classList.remove("d-none");
 });
 
 var searchForm =  $_(".js-search-form");
-var searchInput =  $_(".js-search-input" , searchForm);
+var elSearchInput =  $_(".js-search-input" , searchForm);
+var elMinimumRating = $_(".js-rating-input" , searchForm);
+
+categoriesArray = [];
+var elSelectCategorie = $_(".js-categorie-select" , searchForm);
+normalizeMovies.forEach(function(movie){
+  movie.categories.forEach(function(categorie){
+    if(!(categoriesArray.includes(categorie))){
+      categoriesArray.push(categorie);
+      var elNewOption = createElement("option" , "" , categorie);
+      elNewOption.value = categorie ;
+      elSelectCategorie.appendChild(elNewOption);
+    };
+  });
+});
 
 searchForm.addEventListener('submit' , function(evt){
   evt.preventDefault();
   
-  if(searchInput.value.trim() === ""){
-    searchInput.value = "";
-    searchInput.focus();
-    alert('Birodar biror so`z yozing');
+  if(elSearchInput.value.trim() === "" || elMinimumRating.value.trim() === ""){
+    if(elSearchInput.value.trim() === ""){
+      elSearchInput.value = "";
+      elSearchInput.focus();
+    }else{
+      elMinimumRating.value = "";
+      elMinimumRating.focus();
+    }
+    alert('Birodar biror nima yozing');
     return;
   }
   
-  var searchInputValue = searchInput.value.trim().split(" ");
+  if(isNaN(elMinimumRating.value)){
+    elMinimumRating.value = "";
+    elMinimumRating.focus();
+    alert('Birodar son yozing');
+    return;
+  }
+  
+  var searchInputValue = elSearchInput.value.trim().split(" ");
   var removeNull = function (array) {
     if (array.includes("")){
       array.splice(array.indexOf("") , 1);
@@ -96,33 +115,32 @@ searchForm.addEventListener('submit' , function(evt){
   };
   removeNull(searchInputValue);
   
-  searchInputValue = searchInputValue.join("|").toString();
   
-  var searchResult = [];
+  searchInputValue = searchInputValue.join("|").toString();
   var searchQuery = new RegExp(searchInputValue , "gi");
   
-  normalizeMovies.forEach(function(movie){
-    if(movie.title.toString().match(searchQuery)){
-      searchResult.push(movie);
-    };
+  var categoriesValue = elSelectCategorie.value;
+  var minimumRatingValue= parseFloat(elMinimumRating.value.trim() , 10);
+  
+  var searchResult = normalizeMovies.filter(function(movie){
+    var checkTitle = movie.title.toString().match(searchQuery);
+    var checkCategorie = movie.categories.includes(categoriesValue);
+    var checkIMdBrating = movie.imdbRating >= minimumRatingValue ;
+    return (checkTitle && checkCategorie && checkIMdBrating);
   });
   
   if(searchResult.length === 0){
     alert('Hurmatli foydalanuvchi , afsuski bizda bunday kino yo`q ekan :(( ');
-    renderMovies(twoHundredVideos);
+    renderMovies(twoHundredMovies);
     searchInput.value = "";
     searchInput.focus();
-    btnAddMovies.classList.add("d-block");
-    btnAddMovies.classList.remove("d-none");
+    btnShowMovies.classList.add("d-block");
+    btnShowMovies.classList.remove("d-none");
   }else{
     renderMovies(searchResult);
-    
-    btnAddMovies.classList.add("d-none");
-    btnAddMovies.classList.remove("d-block");
+    btnShowMovies.classList.add("d-none");
+    btnShowMovies.classList.remove("d-block");
     btnReloadMovies.classList.remove("d-none");
     btnReloadMovies.classList.add("d-block");
   }
-  
-  
-  
 });
