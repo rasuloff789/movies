731247d7fdaacc70eacc4819be77d18122a5194c
rasuloff789 <rasuloff789@gmail.com>
2020-11-22 20:34:01 +0500

    util.js probels deleted

--- js/util.js
+++ js/util.js
@@ -1,18 +1,14 @@
 var $_ = function(selector ,  parent = document){
   return  parent.querySelector(selector);
 };
-
 var $$_ = function(selector , parent = document){
   return  parent.querySelectorAll(selector);
 };
-
-
 var createElement = function (tagName, className, text) {
   var element = document.createElement(tagName);
   element.setAttribute('class', className);
   if (text) {
     element.textContent = text;
   }
-  
   return element;
 };
